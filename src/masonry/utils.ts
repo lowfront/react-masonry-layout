c3f27d3362ff5cc83c7e6cb2411e20844e109e42
lowfront <fronterior@hotmail.com>
2022-05-25 00:15:40 +0900

    add get scrollbar size function

--- src/masonry/utils.ts
+++ src/masonry/utils.ts
@@ -5,3 +5,15 @@ export function debounce<T extends any[]>(f: (...args: T) => void, ms: number) {
     timer = setTimeout((...args: T) => f(...args), ms, ...args);
   };
 }
+
+export function getScrollbarSize() {
+  const el = document.createElement('div') as HTMLElement;
+  let scrollbarSize = 0;
+  el.style.overflow = 'scroll';
+  el.style.position = 'absolute';
+  el.style.opacity = '0';
+  document.body.appendChild(el);
+  scrollbarSize = (el.offsetWidth - el.clientWidth) || 0;
+  document.body.removeChild(el);
+  return scrollbarSize;
+};
